a76034c04e317d376f93157c0146cd512e6e523b
Raziq Din <ajiqdin646@gmail.com>
2026-05-09 17:12:18 +0800

    added comments on repository pattern

--- Services/Repository/IProductRepository.cs
+++ Services/Repository/IProductRepository.cs
@@ -8,7 +8,8 @@ using RetailECommerce.Models;
 // 2. it provides a way to manage the data access logic in another class , easier to maintain and test.
 // 3. we can use this pattern to switch between different data sources (e.g. SQL Server, MongoDB, etc.) without changing the business logic layer.
 // 4. business logic layer in this case is the controller and data access layer is the repository ( this class for example)
-
+// in a nutshell : this will talk to the database and act as the middle man between controller and database
+// controller is used to only handle the http requests and responses and call related repository methods
 
 
 public interface IProductRepository
